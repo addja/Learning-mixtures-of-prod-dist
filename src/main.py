3792f04ac0b7136a04f9efd09e1e8d8ecdbb5756
user <daniel.oteroavalle@gmail.com>
2018-03-07 20:57:26 +0000

    Freund and Mansour algorithm halfway implemented

--- src/main.py
+++ src/main.py
@@ -5,18 +5,22 @@ from sfod import ServeidoFeldmanODonnell
 from pd import prodDistGen, prodDistSample
 
 # genrate product distribution
-k = 3   # number of product distributions
-n = 2   # lenght of product distributions
+k = 2   # number of product distributions
+n = 20  # lenght of product distributions
 b = 2   # values prod dist can take
 dist, rates = prodDistGen(k,n,b)
-print('distribution:',dist,'\n')
-print('mixing rates:',rates,'\n')
+# print('distribution:',dist,'\n')
+# print('mixing rates:',rates,'\n')
 
 # sample from product distribution
 m = 20  # number of samples
 samples = prodDistSample(dist,rates,m)
-print('samples:',samples,'\n')
+# print('samples:',samples,'\n')
 
-# FreundMansour()
+# apply Freund Mansour algorithm
+# only for mixtures of two binary product distributions!
+epsilon = 0.1
+delta = 0.01
+FreundMansour(epsilon,delta,samples)
 
 # ServeidoFeldmanODonnell()
